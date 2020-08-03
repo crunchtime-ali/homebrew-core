4ad1a4e8f09dab81e190d83a08029a192b627d74
Alexander Zigelski <ali@crunchtime.dev>
2020-08-03 17:29:58 +0200

    pdftoipe: revision bump for poppler

--- Formula/pdftoipe.rb
+++ Formula/pdftoipe.rb
@@ -4,7 +4,7 @@ class Pdftoipe < Formula
   url "https://github.com/otfried/ipe-tools/archive/v7.2.13.1.tar.gz"
   sha256 "c8725d78e43b7d6a04465e8a156bc4c6a78121f291aac74e0b0a10286ef95544"
   license "GPL-2.0"
-  revision 7
+  revision 8
 
   bottle do
     cellar :any
