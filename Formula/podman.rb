489f83fa39c1cf90978ab3da429343de44bdbd63
Alexander Frisch <crunchtime-ali@users.noreply.github.com>
2022-11-10 18:30:16 +0000

    podman 4.3.1

--- Formula/podman.rb
+++ Formula/podman.rb
@@ -1,8 +1,8 @@
 class Podman < Formula
   desc "Tool for managing OCI containers and pods"
   homepage "https://podman.io/"
-  url "https://github.com/containers/podman/archive/v4.3.0.tar.gz"
-  sha256 "55a3a09b80f23f78aaeb74fbf878fa0b1ef1842e5b282ad61e82a9dc4c74bf34"
+  url "https://github.com/containers/podman/archive/v4.3.1.tar.gz"
+  sha256 "455c29c4ee78cd6365e5d46e20dd31a5ce4e6e1752db6774253d76bd3ca78813"
   license all_of: ["Apache-2.0", "GPL-3.0-or-later"]
   head "https://github.com/containers/podman.git", branch: "main"
 
